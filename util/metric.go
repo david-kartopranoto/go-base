5c175d330b5c164cdb5e10145fd3c68d0a0c4527
Daka <dkartopr@gmail.com>
2021-09-16 01:15:36 +0700

    rename metric service and remove unused histogram data

--- util/metric.go
+++ util/metric.go
@@ -7,20 +7,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-//service implements Service interface
-type service struct {
-	httpRequestHistogram *prometheus.HistogramVec
-}
-
-type HistogramData struct {
-	Handler    string
-	Method     string
-	StatusCode string
-	Duration   float64
+//MetricService implements MetricService interface
+type MetricService struct {
+	httpReq *prometheus.HistogramVec
 }
 
 //NewPrometheusService create a new prometheus service
-func NewPrometheusService() (*service, error) {
+func NewPrometheusService() (*MetricService, error) {
 	http := prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: "http",
 		Name:      "request_duration_seconds",
@@ -28,21 +21,21 @@ func NewPrometheusService() (*service, error) {
 		Buckets:   prometheus.DefBuckets,
 	}, []string{"handler", "method", "code"})
 
-	s := &service{
-		httpRequestHistogram: http,
+	s := &MetricService{
+		httpReq: http,
 	}
-	err := prometheus.Register(s.httpRequestHistogram)
-	if err != nil && err.Error() != "duplicate metrics collector registration attempted" {
+	err := prometheus.Register(s.httpReq)
+	if err != nil {
 		return nil, err
 	}
+
 	return s, nil
 }
 
-func (s *service) Handler() http.Handler {
+func (s *MetricService) Handler() http.Handler {
 	return promhttp.Handler()
 }
 
-//SaveHTTP send metrics to server
-func (s *service) SaveHistogram(handler, method, statusCode string, duration float64) {
-	s.httpRequestHistogram.WithLabelValues(handler, method, statusCode).Observe(duration)
+func (s *MetricService) SaveHistogram(handler, method, statusCode string, duration float64) {
+	s.httpReq.WithLabelValues(handler, method, statusCode).Observe(duration)
 }
